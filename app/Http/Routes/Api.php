bf278dd5c9000d993c019bdf08d94584bbfcff74
Matt Withoos <mattwithoos@gmail.com>
2019-03-16 15:43:09 +1100

    Adding routes for new Member Mailchimp API feature

--- app/Http/Routes/Api.php
+++ app/Http/Routes/Api.php
@@ -5,11 +5,18 @@ declare(strict_types=1);
 
 // MailChimp group
 $router->group(['prefix' => 'mailchimp', 'namespace' => 'MailChimp'], function () use ($router) {
-    // Lists group
+    // Lists & members
     $router->group(['prefix' => 'lists'], function () use ($router) {
+        // Lists routes
         $router->post('/', 'ListsController@create');
         $router->get('/{listId}', 'ListsController@show');
         $router->put('/{listId}', 'ListsController@update');
         $router->delete('/{listId}', 'ListsController@remove');
+
+        // Members routes, following MailChimp's URL style
+        $router->post('/{listId}/members', 'MembersController@create');
+        $router->get('/{listId}/members/{subscriberHash}', 'MembersController@show');
+        $router->put('/{listId}/members/{subscriberHash}', 'MembersController@update');
+        $router->delete('/{listId}/members/{subscriberHash}', 'MembersController@remove');
     });
 });
