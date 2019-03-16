911ed88e9678402e43bb7c56cee639d8204550a7
Matt Withoos <mattwithoos@gmail.com>
2019-03-16 15:53:26 +1100

    Database mapping for new Member feature for Mailchimp API

---
+++ app/Database/Entities/MailChimp/MailChimpMember.php
@@ -0,0 +1,397 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Database\Entities\MailChimp;
+
+use Doctrine\ORM\Mapping as ORM;
+use EoneoPay\Utils\Str;
+
+/**
+ * @ORM\Entity()
+ */
+class MailChimpMember extends MailChimpEntity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\Column(name="id", type="guid")
+     * @ORM\GeneratedValue(strategy="UUID")
+     *
+     * @var string
+     */
+    private $memberId;
+
+    /**
+     * @ORM\Column(name="email_address", type="string")
+     *
+     * @var string
+     */
+    private $emailAddress;
+
+    /**
+     * @ORM\Column(name="email_type", type="string")
+     *
+     * @var string
+     */
+    private $emailType;
+
+    /**
+     * @ORM\Column(name="status", type="string")
+     *
+     * @var string
+     */
+    private $status;
+
+    /**
+     * @ORM\Column(name="merge_fields", type="array")
+     *
+     * @var array
+     */
+    private $mergeFields;
+
+    /**
+     * @ORM\Column(name="interests", type="array")
+     *
+     * @var array
+     */
+    private $interests;
+
+    /**
+     * @ORM\Column(name="language", type="string")
+     *
+     * @var string
+     */
+    private $language;
+
+    /**
+     * @ORM\Column(name="vip", type="boolean")
+     *
+     * @var boolean
+     */
+    private $vip;
+
+    /**
+     * @ORM\Column(name="location", type="array")
+     *
+     * @var array
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(name="marketing_permissions", type="array")
+     *
+     * @var array
+     */
+    private $marketingPermissions;
+
+    /**
+     * @ORM\Column(name="ip_signup", type="string")
+     *
+     * @var string
+     */
+    private $ipSignup;
+
+    /**
+     * @ORM\Column(name="timestamp_signup", type="string")
+     *
+     * @var string
+     */
+    private $timestampSignup;
+
+    /**
+     * @ORM\Column(name="ip_opt", type="string")
+     *
+     * @var string
+     */
+    private $ipOpt;
+
+    /**
+     * @ORM\Column(name="timestamp_opt", type="string")
+     *
+     * @var string
+     */
+    private $timestampOpt;
+
+    /**
+     * @ORM\Column(name="tags", type="array")
+     *
+     * @var array
+     */
+    private $tags;
+
+    /**
+     * Get id.
+     *
+     * @return null|string
+     */
+    public function getId(): ?string
+    {
+        return $this->memberId;
+    }
+
+    /**
+     * Get mailchimp id of the member.
+     *
+     * @return null|string
+     */
+    public function getMailChimpId(): ?string
+    {
+        return $this->mailChimpId;
+    }
+
+    /**
+     * Get validation rules for mailchimp entity.
+     *
+     * @return array
+     */
+    public function getValidationRules(): array
+    {
+        return [
+            'email_address' => 'required|string',
+            'email_type' => 'required|string',
+            'status' => 'required|string',
+            'merge_fields' => 'required|array',
+            'interests' => 'required|array',
+            'language' => 'required|string',
+            'vip' => 'required|boolean',
+            'location' => 'required|array',
+            'location.longitude' => 'required|decimal',
+            'location.latitude' => 'required|decimal',
+            'marketing_permissions' => 'nullable|array',
+            'marketing_permissions.marketing_permission_id' => 'nullable|string',
+            'marketing_permissions.enabled' => 'nullable|boolean',
+            'ip_signup' => 'required|string',
+            'timestamp_signup' => 'required|string',
+            'ip_opt' => 'required|string',
+            'timestamp_opt' => 'required|string',
+            'tags' => 'nullable|array'
+        ];
+    }
+
+    /**
+     * Set mailchimp id of the member.
+     *
+     * @param string $mailChimpId
+     *
+     * @return \App\Database\Entities\MailChimp\MailChimpMember
+     */
+    public function setMailChimpId(string $mailChimpId): MailChimpMember
+    {
+        $this->mailChimpId = $mailChimpId;
+
+        return $this;
+    }
+
+    /**
+     * Set email address.
+     *
+     * @param string $emailAddress
+     *
+     * @return MailChimpMember
+     */
+    public function setEmailAddress(string $emailAddress): MailChimpMember
+    {
+        $this->emailAddress = $emailAddress;
+
+        return $this;
+    }
+
+    /**
+     * Set email type.
+     *
+     * @param string $emailType
+     *
+     * @return MailChimpMember
+     */
+    public function setEmailType(string $emailType): MailChimpMember
+    {
+        $this->emailType = $emailType;
+
+        return $this;
+    }
+
+    /**
+     * Set status.
+     *
+     * @param string $status
+     *
+     * @return MailChimpMember
+     */
+    public function setStatus(string $status): MailChimpMember
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Set merge fields.
+     *
+     * @param array $mergeFields
+     *
+     * @return MailChimpMember
+     */
+    public function setMergeFields(array $mergeFields): MailChimpMember
+    {
+        $this->mergeFields = $mergeFields;
+
+        return $this;
+    }
+
+    /**
+     * Set interests.
+     *
+     * @param array $interests
+     *
+     * @return MailChimpMember
+     */
+    public function setInterests(array $interests): MailChimpMember
+    {
+        $this->interests = $interests;
+
+        return $this;
+    }
+
+    /**
+     * Set language.
+     *
+     * @param string $language
+     *
+     * @return MailChimpMember
+     */
+    public function setLanguage(string $language): MailChimpMember
+    {
+        $this->language = $language;
+
+        return $this;
+    }
+
+    /**
+     * Set vip.
+     *
+     * @param bool $vip
+     *
+     * @return MailChimpMember
+     */
+    public function setVip(bool $vip): MailChimpMember
+    {
+        $this->vip = $vip;
+
+        return $this;
+    }
+
+    /**
+     * Set location.
+     *
+     * @param array $location
+     *
+     * @return MailChimpMember
+     */
+    public function setLocation(array $location): MailChimpMember
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Set marketing permissions.
+     *
+     * @param array $marketingPermissions
+     *
+     * @return MailChimpMember
+     */
+    public function setMarketingPermissions(array $marketingPermissions): MailChimpMember
+    {
+        $this->marketingPermissions = $marketingPermissions;
+
+        return $this;
+    }
+
+    /**
+     * Set ip signup.
+     *
+     * @param string $ipSignup
+     *
+     * @return MailChimpMember
+     */
+    public function setIpSignup(string $ipSignup): MailChimpMember
+    {
+        $this->ipSignup = $ipSignup;
+
+        return $this;
+    }
+
+    /**
+     * Set timestamp signup.
+     *
+     * @param string $timestampSignup
+     *
+     * @return MailChimpMember
+     */
+    public function setTimestampSignup(string $timestampSignup): MailChimpMember
+    {
+        $this->timestampSignup = $timestampSignup;
+
+        return $this;
+    }
+
+    /**
+     * Set ip opt.
+     *
+     * @param string $ipOpt
+     *
+     * @return MailChimpMember
+     */
+    public function setIpOpt(string $ipOpt): MailChimpMember
+    {
+        $this->ipOpt = $ipOpt;
+
+        return $this;
+    }
+
+    /**
+     * Set timestamp opt.
+     *
+     * @param string $timestampOpt
+     *
+     * @return MailChimpMember
+     */
+    public function setTimestampOpt(string $timestampOpt): MailChimpMember
+    {
+        $this->timestampOpt = $timestampOpt;
+
+        return $this;
+    }
+
+    /**
+     * Set tags.
+     *
+     * @param array $tags
+     *
+     * @return MailChimpMember
+     */
+    public function setTags(array $tags): MailChimpMember
+    {
+        $this->tags = $tags;
+
+        return $this;
+    }
+
+
+    /**
+     * Get array representation of entity.
+     *
+     * @return array
+     */
+    public function toArray(): array
+    {
+        $array = [];
+        $str = new Str();
+
+        foreach (\get_object_vars($this) as $property => $value) {
+            $array[$str->snake($property)] = $value;
+        }
+
+        return $array;
+    }
+}
