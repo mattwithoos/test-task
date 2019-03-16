a20cee592b62fd338962a89879f219938941711a
Matt Withoos <mattwithoos@gmail.com>
2019-03-16 16:07:34 +1100

    Members are not reliant on list ID except for creation so changing grouping

--- app/Http/Routes/Api.php
+++ app/Http/Routes/Api.php
@@ -5,18 +5,20 @@ declare(strict_types=1);
 
 // MailChimp group
 $router->group(['prefix' => 'mailchimp', 'namespace' => 'MailChimp'], function () use ($router) {
-    // Lists & members
+    // Lists group
     $router->group(['prefix' => 'lists'], function () use ($router) {
         // Lists routes
         $router->post('/', 'ListsController@create');
         $router->get('/{listId}', 'ListsController@show');
         $router->put('/{listId}', 'ListsController@update');
         $router->delete('/{listId}', 'ListsController@remove');
-
+    });
+    // Members group
+    $router->group(['prefix' => 'members'], function () use ($router) {
         // Members routes, following MailChimp's URL style
-        $router->post('/{listId}/members', 'MembersController@create');
-        $router->get('/{listId}/members/{memberId}', 'MembersController@show');
-        $router->put('/{listId}/members/{memberId}', 'MembersController@update');
-        $router->delete('/{listId}/members/{memberId}', 'MembersController@remove');
+        $router->post('/', 'MembersController@create');
+        $router->get('/{memberId}', 'MembersController@show');
+        $router->put('/{memberId}', 'MembersController@update');
+        $router->delete('/{memberId}', 'MembersController@remove');
     });
 });
